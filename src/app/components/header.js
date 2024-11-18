474df9962eb9f4f4147886d3d7e8f18ea358b80e
Sachin Parihar <pariharsachin5002@gmail.com>
2024-11-18 21:49:33 +0530

    glitch Effect

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -8,18 +8,17 @@ const Header = () => {
 
   useEffect(() => {
     const options = {
-      strings: ["gambit"], // Texts to type
-      typeSpeed: 100, // Typing speed in milliseconds
-      backSpeed: 50, // Backspacing speed in milliseconds
-      showCursor: true, // Show typing cursor
-      cursorChar: "|", // Cursor character
-      loop: true, // Loop the animation
+      strings: ["gambit"],
+      typeSpeed: 100,
+      backSpeed: 50,
+      showCursor: true,
+      cursorChar: "|",
+      loop: true,
     };
 
     const typed = new Typed(typedRef.current, options);
 
     return () => {
-      // Destroy Typed.js instance to prevent memory leaks
       typed.destroy();
     };
   }, []);
@@ -28,20 +27,20 @@ const Header = () => {
     <header className="w-full px-4 sm:px-6 h-[12vh] py-3 sm:py-4 flex justify-between items-center">
       {/* Logo Section */}
       <div className="flex items-center">
-        <div className=" sm:text-2xl flex items-center gap-2 text-lg jetbrains-mono-font font-light text-green-200">
+        <div className="sm:text-2xl flex items-center gap-2 text-lg jetbrains-mono-font font-light text-green-200">
           &gt;_
           <span
-            className="text-lg jetbrains-mono-font font-light text-green-200"
+            className="relative text-lg jetbrains-mono-font font-light text-green-200 glitch-effect"
             ref={typedRef}
           ></span>
         </div>
       </div>
 
       <button className="bg-black transition-all duration-300 ease-in-out helveticaNewRoman text-white px-3 py-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-2 text-sm sm:text-base hover:bg-gradient-to-r hover:from-green-500 hover:to-green-700 hover:scale-105 hover:shadow-lg">
-  <span className="flex gap-x-1 items-center jetbrains-mono-font justify-center">
-    Connect <FaTelegramPlane />
-  </span>
-</button>
+        <span className="flex gap-x-1 items-center jetbrains-mono-font justify-center">
+          Connect <FaTelegramPlane />
+        </span>
+      </button>
     </header>
   );
 };
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,12 +1,20 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect } from 'react';
 import { FaTwitter, FaTelegramPlane } from "react-icons/fa";
 import Header from '../components/header';
 import { SiGitbook } from "react-icons/si";
 import Footer from '../components/footer';
 import Logo from "../components/assets/logo.png"
 import Image from 'next/image';
+import { PowerGlitch } from 'powerglitch';
 
 const MainPage = () => {
+  useEffect(() => {
+    // Apply glitch effect to the element with the 'glitchIt' class
+    PowerGlitch.glitch('.glitchIt');
+  }, []);
+
   return (
     <div className="w-full min-h-screen gridBG flex  items-center flex-col text-white">
       <Header />
@@ -14,14 +22,14 @@ const MainPage = () => {
         <div className="text-center space-y-6 mb-12 sm:mb-20 flex flex-col justify-center items-center">
           <Image className='h-[18vh] w-auto' src={Logo} alt='logo'/>
           <div className="inline-flex items-center gap-2 rounded-full bg-gradient-to-r from-green-500 to-green-700 px-4 py-1 text-xs sm:text-sm font-semibold text-white shadow-lg ring-2 ring-green-600/50">
-  <span className="uppercase tracking-wide">Early Access</span>
-</div>
+            <span className="uppercase tracking-wide">Early Access</span>
+          </div>
 
-          <h1 className="text-6xl text-green-200 tracking-tighter sm:text-5xl md:text-7xl font-bold jetbrains-mono-font leading-tight">
+          <h1 className="text-6xl text-green-200 glitchIt tracking-tighter sm:text-5xl md:text-7xl font-bold jetbrains-mono-font leading-tight">
             Play The Market
           </h1>
           <p className="text-lg  md:text-xl text-gray-300 jetbrains-mono-font font-extralight tracking-tighter">
-          Every trade is a strategic play<br className="hidden sm:block" />Snipe the market with precision
+            Every trade is a strategic play<br className="hidden sm:block" />Snipe the market with precision
           </p>
           
           <div className="flex justify-center bg-zinc-400/20 w-fit gap-1 sm:gap-2 mt-6 sm:mt-8 rounded-full p-1">
